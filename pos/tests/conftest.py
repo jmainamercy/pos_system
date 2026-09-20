f2cd93d2ab828bbb30beb8425a67919228708b4e
Mercy Maina <jmainamercy@gmail.com>
2026-09-21 00:30:20 +0300

    added tests

--- pos/tests/conftest.py
+++ pos/tests/conftest.py
@@ -1,53 +1,6 @@
-# import os
-# import pytest
-# from fastapi.testclient import TestClient
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.pool import StaticPool
-
-# os.environ["DATABASE_URL"] = "sqlite://"
-
-# from database import Base, get_db
-# from main import app
-
-# from dependencies import get_current_user 
-
-# engine = create_engine("sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool)
-# TestingSessionLocal = sessionmaker(bind=engine)
-
-# class MockUser:
-#     id = 1
-#     username = "John"
-#     masked_password = "hashed_dummy_string"
-#     full_name = "John Doe"
-#     role = "admin"       
-#     is_active = True    
-
-# @pytest.fixture
-# def client():
-#     Base.metadata.create_all(bind=engine)
-
-#     def override_get_db():
-#         db = TestingSessionLocal()
-#         try:
-#             yield db
-#         finally:
-#             db.close()
-
-#     app.dependency_overrides[get_db] = override_get_db
-
-#     def override_get_current_user():
-#         return MockUser()
-        
-#     app.dependency_overrides[get_current_user] = override_get_current_user
-
-#     yield TestClient(app)
-
-#     app.dependency_overrides.clear()
-#     Base.metadata.drop_all(bind=engine)
 import os
 import pytest
-from datetime import datetime  # Added to handle database timestamp fields
+from datetime import datetime 
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, DateTime
 from sqlalchemy.orm import sessionmaker
